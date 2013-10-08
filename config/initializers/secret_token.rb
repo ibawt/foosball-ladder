cdc3291e5b7509429dc18c9fb6e6342aa89147f4
Ian Quick <ian.quick@gmail.com>
2013-10-08 07:50:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoosballLadder::Application.config.secret_token = 'a5f2330116cd02760511fb9f2315a6bfa5aeb70e1f8cb7b785720d2e887e6744f94330264b803b41b3cbb13325dce28de7c87a3ed253184276799863c20c3e00'
